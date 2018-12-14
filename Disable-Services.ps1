00fe8d0d6922987b9164339f19df89880d0ea8ae
Ignacio <45845856+Ignaciox@users.noreply.github.com>
2018-12-14 14:51:36 +0100

    Update Disable-Services.ps1

--- Disable-Services.ps1
+++ Disable-Services.ps1
@@ -4,22 +4,13 @@
 # some services may just come back on after a windows update.
 
 $services = @(
-    "diagnosticshub.standardcollector.service" # Microsoft (R) Diagnostics Hub Standard Collector Service
+    "diagnosticshub.standardcollector.service" # Diagnostics Hub Service
     "DiagTrack"                                # Diagnostics Tracking Service
     "dmwappushservice"                         # WAP Push Message Routing Service
-    "HomeGroupListener"                        # HomeGroup Listener
-    "HomeGroupProvider"                        # HomeGroup Provider
-    "lfsvc"                                    # Geolocation Service
-    "MapsBroker"                               # Downloaded Maps Manager
-    "NetTcpPortSharing"                        # Net.Tcp Port Sharing Service
-    "RemoteAccess"                             # Routing and Remote Access
-    "RemoteRegistry"                           # Remote Registry
     "SharedAccess"                             # Internet Connection Sharing 
-    "TrkWks"                                   # Distributed Link Tracking Client
     "WbioSrvc"                                 # Windows Biometric Service
     #"WlanSvc"                                 # WLAN 
     "WMPNetworkSvc"                            # Windows Media Player Network Sharing
-    "wscsvc"                                   # Windows Security Center Service
     #"WSearch"                                 # Windows Search
     "XblAuthManager"                           # Xbox Live Auth Manager
     "XblGameSave"                              # Xbox Live Game Save Service
